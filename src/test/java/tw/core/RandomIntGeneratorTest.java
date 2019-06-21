e8c469122e524926539958b54d3c1f426f0d028b
woodpecker <czq.woodpecker@qq.com>
2019-06-21 23:46:31 +0800

    finish RandomIntGeneratorTest

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,32 @@
 package tw.core;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
 
+    @Test
+    public void generateNumsTest() {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        String numsStr = randomIntGenerator.generateNums(9, 4);
+        String[] numsArray = numsStr.split(" ");
+        Assert.assertEquals(4, numsArray.length);
+        boolean result = true;
+        for (int i = 0; i < numsArray.length; i++) {
+            try {
+                int number = Integer.parseInt(numsArray[i]);
+                if (number > 9) {
+                    result = false;
+                }
+            } catch (NumberFormatException e) {
+                result = false;
+            }
+        }
+        Assert.assertEquals(true, result);
+    }
 }
