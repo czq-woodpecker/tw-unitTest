abb5c142e932333a1eb0000bd95b3ef06e418076
woodpecker <czq.woodpecker@qq.com>
2019-06-22 00:02:43 +0800

    finish AnswerGeneratorTest

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,42 @@
 package tw.core.generator;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+    RandomIntGenerator randomIntGenerator;
+
+    @Before
+    public void init() {
+        randomIntGenerator = Mockito.mock(RandomIntGenerator.class);
+        Mockito.when(randomIntGenerator.generateNums(9, 4)).thenReturn("1 2 3 4");
+    }
+
+    @Test
+    public void generateTest() throws OutOfRangeAnswerException {
+        String answerStr = new AnswerGenerator(randomIntGenerator).generate().toString();
+        String[] numStrs = answerStr.split(" ");
+        Assert.assertEquals(4, numStrs.length);
+        boolean result = true;
+        try {
+            for (int i = 0; i < numStrs.length; i++) {
+                int number = Integer.parseInt(numStrs[i]);
+                if(number > 9) {
+                    result = false;
+                }
+            }
+        } catch (NumberFormatException e) {
+            result = false;
+        }
+        Assert.assertEquals(true, result);
+    }
 
 }
 
